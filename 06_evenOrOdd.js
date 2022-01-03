888973a68db93188e619a53b7ab91ec71b32b351
Artur Terziyan <ararad@rambler.ru>
2022-01-03 13:54:50 +0300

    feat: add task 06 Even or Odd

---
+++ 06_evenOrOdd.js
@@ -0,0 +1,12 @@
+function even_or_odd(number) {
+  if (number%2 === 0) {
+     return console.log('Even');
+  } else {
+     return console.log('Odd');
+  };
+};
+
+even_or_odd(5);
+even_or_odd(6);
+even_or_odd(123);
+even_or_odd(2000);
