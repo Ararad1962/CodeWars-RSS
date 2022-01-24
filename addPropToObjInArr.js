030c6f1a9f88778d95206b05c965821a7f50fcae
Artur Terziyan <ararad@rambler.ru>
2022-01-24 08:16:53 +0300

    feat: add task addPropToObjInArr

--- addPropToObjInArr.js
+++ addPropToObjInArr.js
@@ -1,13 +1,12 @@
-function addUsersAnswer(arr) {
-   // console.log(arr[0])
-   return arr.map( (item) => { return addPropInObj(item) } );
- }
- function addPropInObj(obj) {
-    obj.usersAnswer = null;
-    return obj;
- } 
-
- const questions = [{
+// function addUsersAnswer(arr) {
+//    // console.log(arr[0])
+//    return arr.map( (item) => { return addPropInObj(item) } );
+//  }
+//  function addPropInObj(obj) {
+//     obj.usersAnswer = null;
+//     return obj;
+//  } 
+let questions = [{
    question: "What's the currency of the USA?",
    choices: ["US dollar", "Ruble", "Horses", "Gold"],
    corAnswer: 0
@@ -17,11 +16,24 @@ function addUsersAnswer(arr) {
    corAnswer: 0
 }];
 
-console.log(addUsersAnswer(questions));
+questions = questions.map((v) => {v.usersAnswer = null;
+   return  v;
+});
+
+
+
+ 
+
+console.log(questions);
 // const obj = {
 //    question: "What's the currency of the USA?",
 //    choices: ["US dollar", "Ruble", "Horses", "Gold"],
 //    corAnswer: 0
 // };
+
+// function addUsersAnswer(arr) {
+//    const arrNew = arr.map(item => item[usersAnswer] = null);
+//    console.log(arrNew);
+//    return arrNew;
 
 // console.log(addPropInObj(obj));
